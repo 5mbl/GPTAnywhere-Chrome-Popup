1f693606a9f40cd25b89789df65e54f672d7a490
serdar.plg <nyrox.plg@gmail.com>
2022-12-18 01:39:29 +0100

    update

--- src/frontend/index.mjs
+++ src/frontend/index.mjs
@@ -2,21 +2,26 @@ import Browser from "webextension-polyfill";
 const searchInput = document.getElementById('input-el');
 const buttonEl = document.getElementById('submit-btn');
 const container = document.getElementById('content-El');
+const xIcon = document.getElementById("x-icon")
 
 async function run(question) { 
-//  container.className = "chat-gpt-container"; 
+  // loading for port Message...
   container.innerHTML = '<p class="loading">Waiting for ChatGPT response...</p>'; 
 
   const port = Browser.runtime.connect(); 
   
+  // port Listener
   port.onMessage.addListener(function (msg) {
+    
     if (msg.answer) {
       console.log("answer: " + msg.answer) // <-- Console.Log 1
 
       container.innerHTML = '<p><span class="prefix">ChatGPT:</span><pre></pre></p>';
-      container.querySelector("pre").textContent = msg.answer; 
+       // Answer Output
+      container.querySelector("pre").textContent = msg.answer;
 
-      //container.innerHTML = '<p>Please Login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
+      // new
+
     } else {
       container.innerHTML = `
       <section class="hero-area">
@@ -24,23 +29,36 @@ async function run(question) {
             <div class="hero-inner-item">
                 <h2>🖖 You are nearly ready!</h2>
                 <p>You need to login into your openAI <br> Account first!</p>
-                <a href="https://chat.openai.com/auth/login">Login</a>
+                <a href="https://chat.openai.com/auth/login" target="_blank">Login</a>
             </div>
         </div>
     </section>
-      
-      
-      `;
+    `;
     }
   });
   port.postMessage({ question });
 
 }
 
-buttonEl.addEventListener("click",()=>{
-  console.log("button clicked");
-    if (searchInput && searchInput.value){
+  // Eventlistener Button - Click
+  buttonEl.addEventListener("click", () => {
+    console.log("button clicked");
+    if (searchInput && searchInput.value) {
       console.log("Input.Value:" + searchInput.value);
-        run(searchInput.value);
+      run(searchInput.value);
     }
-})
+  });
+  // Eventlistener keypress - Enter
+  searchInput.addEventListener("keypress",(event)=>{
+    if(event.key === "Enter"){
+      if (searchInput && searchInput.value) {
+        console.log("Input.Value:" + searchInput.value);
+        run(searchInput.value);
+      }
+      event.preventDefault()
+    }
+  });
+
+  xIcon.addEventListener("click",()=>{
+    window.close();
+  })
